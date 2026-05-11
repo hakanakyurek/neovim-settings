e2bbaa7eaaf37a6050e7dd48bb27a4ad4364a1a5
Hakan Akyürek <hakannakyurek@gmail.com>
2026-05-11 12:55:00 +0200

    feat: Window title should be set to the project name

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.title = true
+
+vim.api.nvim_create_autocmd({ "VimEnter", "DirChanged", "BufEnter" }, {
+  callback = function()
+    vim.opt.titlestring = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.opt.titlestring = ""
+  end,
+})
